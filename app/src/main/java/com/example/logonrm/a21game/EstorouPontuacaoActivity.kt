13c39638b74672ed8d04ee950f82d25af148754d
Otavio <persistere.5@gmail.com>
2018-02-02 14:28:59 -0200

    Inserindo icone de sucesso,som de explosao,  inserindo reultado quando estorou a pontuação

--- app/src/main/java/com/example/logonrm/a21game/EstorouPontuacaoActivity.kt
+++ app/src/main/java/com/example/logonrm/a21game/EstorouPontuacaoActivity.kt
@@ -1,12 +1,28 @@
 package com.example.logonrm.a21game
 
+import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_estorou_pontuacao.*
 
 class EstorouPontuacaoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_estorou_pontuacao)
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+
+        tvPontuacaoFinal.text = intent.getStringExtra("PONTUACAO")
+
+        val mediaPlayer = MediaPlayer.create(this, R.raw.explosao);
+        mediaPlayer.start();
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
 }
